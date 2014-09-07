e51eaa9c40475ba954b22352625ecdcf5473694f
Eric <erjohnson.pdx@gmail.com>
2014-09-06 19:05:10 -0700

    Add locations#new, #create

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,4 +8,22 @@ class LocationsController < ApplicationController
     @location = Location.find params[:id]
     @comments = @location.comments
   end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.create! attributes
+    redirect_to :action => 'show', :id => @location.id
+  end
+
+  private
+    def generate_primary_key
+      params[:location][:name].downcase.tr(': ', '_')
+    end
+
+    def attributes
+      params[:location].merge(:id => generate_primary_key)
+    end
 end
